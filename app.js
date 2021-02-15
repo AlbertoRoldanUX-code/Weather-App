7f4454d41eeccdb3128d2a2f0eab641c81b9a55d
Alberto <a-f-r@hotmail.es>
2021-02-16 00:30:32 +0100

    Add first app.get

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-
+app.get("/", function(req, res){
+  res.send("Hello World!");
+})
 
 
 
@@ -11,5 +13,5 @@ const app = express();
 
 
 app.listen(3000, function(){
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000.");
 });
