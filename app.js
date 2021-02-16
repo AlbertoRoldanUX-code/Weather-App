b2747cc19e0bc168930bcfd426b8c45438a9f657
Alberto <a-f-r@hotmail.es>
2021-02-16 19:34:24 +0100

    Display weather data from Málaga

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const https = require("https");
 require("dotenv").config();
-console.log(process.env);
 
 const app = express();
 
@@ -10,10 +9,14 @@ app.get("/", function(req, res){
   const url = "https://api.openweathermap.org/data/2.5/weather?q=Malaga&units=metric&appid=" + process.env.API_KEY;
 
   https.get(url, function(response){
-    console.log(response.statusCode);
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      res.send("The temperature in Málaga is " + temp + " degrees Celsius.")
+    })
   })
 
-  res.send("Server is up and running");
 })
 
 
