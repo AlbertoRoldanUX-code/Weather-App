0663e22b8a56e254f0875167e9958d5832b1f2ea
Alberto <a-f-r@hotmail.es>
2021-02-16 23:07:59 +0100

    Create variable and update post request

--- app.js
+++ app.js
@@ -5,37 +5,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html")
 
 
 })
 
-app.post("/", function(req, res){
-  res.send("Thanks for posting that!")
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + process.env.API_KEY;
+
+  https.get(url, function(response){
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently " + description + "</p>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius.</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send()
+    })
+  })
 })
 
 
-// const query = "London";
-// const unit = "metric";
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + process.env.API_KEY;
-//
-// https.get(url, function(response){
-//   response.on("data", function(data){
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const description = weatherData.weather[0].description;
-//     const icon = weatherData.weather[0].icon
-//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-//     res.write("<p>The weather is currently " + description + "</p>");
-//     res.write("<h1>The temperature in Malaga is " + temp + " degrees Celsius.</h1>");
-//     res.write("<img src=" + imageURL + ">");
-//     res.send()
-//   })
-// })
 
 
 
@@ -43,6 +44,7 @@ app.post("/", function(req, res){
 
 
 
-app.listen(3000, function(){
+
+app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
