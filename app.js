bcf45118bd709ea5ac29e81c2c2109e59a9df507
Alberto <a-f-r@hotmail.es>
2021-02-16 18:58:09 +0100

    Use dotenv package to hide API key

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const https = require("https");
+require("dotenv").config();
+console.log(process.env);
 
 const app = express();
 
-var mykey = config.MY_KEY;
 
 app.get("/", function(req, res){
-
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Malaga&units=metric&appid=" + mykey;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Malaga&units=metric&appid=" + process.env.API_KEY;
 
   https.get(url, function(response){
-    console.log(response);
+    console.log(response.statusCode);
   })
 
-  res.send("Hello World!");
+  res.send("Server is up and running");
 })
 
 
