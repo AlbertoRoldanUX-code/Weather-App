0653030aff6e4fc334f2cc3ecea557907d03e74a
Alberto <a-f-r@hotmail.es>
2021-02-16 13:14:11 +0100

    Add .gitignore and hide API key

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
+var mykey = config.MY_KEY;
+
 app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Malaga&units=metric&appid=" + mykey;
+
+  https.get(url, function(response){
+    console.log(response);
+  })
+
   res.send("Hello World!");
 })
 
